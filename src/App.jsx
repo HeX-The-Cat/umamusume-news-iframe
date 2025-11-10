4d7e49ac75f61184624ad0202b3e24d5e5b59fbc
HeX The Cat <60222984+HeX-The-Cat@users.noreply.github.com>
2025-11-10 09:17:00 +0200

    pub

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className=" w-screen h-screen flex justify-center text-center">
+        <div className=" bg-amber-200 w-2/5 h-screen">
+          <iframe className=" w-full h-full" src="https://webview.games.umamusume.com/umamusume/contents/v/index.html#/info?p=4&c=0"></iframe>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
